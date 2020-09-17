d931ea845773d2b86fceee1bb0217f9db7b062de
see-bet-code <brittani.elan.taylor@gmail.com>
2020-09-17 11:27:42 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,23 @@
 class Customer
-  
+    attr_accessor :name, :age, :meals, :waiters
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+        @meals = []
+        @waiters = []
+    end
+
+    def self.all
+        @@all
+    end
+    
+    def new_meal(waiter, total, tip)
+        @waiters << waiter
+        new_meal = Meal.new(waiter, self, total, tip)  
+        waiter.meals << new_meal
+        @meals << new_meal
+    end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,25 @@
-class Waiter
+require 'pry'
 
+class Waiter
+    attr_accessor :name, :years, :meals
+    @@all = []
+
+    def initialize(name, years)
+        @name = name
+        @years = years
+        @@all << self
+        @meals = []
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        customer.new_meal(self, total, tip)
+    end
+
+    def best_tipper
+        @meals.max_by { |meal| meal.tip }.customer
+    end
 end
